503574820d9adaa7119aca3d26ad4a82af186ffe
FERDINAND-BASSOLE <dibazinferdinand7@gmail.com>
2026-04-13 15:32:55 +0000

    mise à jour is admin

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,9 +15,11 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'phone_number' => $this->phone_number,
+            'is_admin' => $this->isAdmin(), // Utilise la méthode existante du modèle
         ];
     }
 }
