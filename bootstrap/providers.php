e9f5a5a2cf8bbd45994bfe3d903cb2ab625224de
blaisebouda <blaisebouda05@gmail.com>
2026-04-16 15:51:32 +0000

    feat :  filament installation

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-use App\Providers\TelescopeServiceProvider;
-
 return [
-    AppServiceProvider::class,
-    TelescopeServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
