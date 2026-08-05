1c2053a52b33dfa4decf28b95920a60d2ba5ff3f
blaisebouda <blaisebouda05@gmail.com>
2026-08-05 12:10:44 +0000

    fix : session

--- resources/js/lib/api.ts
+++ resources/js/lib/api.ts
@@ -1,12 +1,26 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:8000/api"
+const BASE_URL = "https://zilopay.onrender.com"
 
 export const baseApi = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
+  withCredentials: true,
   headers: {
-    Accept: "application/json",
+    'X-Requested-With': 'XMLHttpRequest',
+    'Accept': 'application/json',
+    'X-Inertia': 'true',
+  },
+})
+
+const api = axios.create({
+  baseURL: BASE_URL + '/api',
+  timeout: 10000,
+  withCredentials: true,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Accept': 'application/json',
+    'X-Inertia': 'true',
   },
 })
 
